9de1b80b48c852ef1bfec8ae0cd3ae7647cc0191
sadhin28 <taosifsadhin113@gmail.com>
2025-03-28 02:28:37 +0600

    string declaration practice

---
+++ string.js
@@ -0,0 +1,17 @@
+
+//string declaration
+// const country = 'Bangladesh';
+// const division = "Barishal";
+// const district = `B-Baria`;
+// const thana = new String('Demra');
+// console.log(typeof thana);
+// console.log(thana);
+
+const number =[1,3,5,6,7,3,23,4,5,45,2342,54646];
+// console.log(number.length);
+let capital = 'Dhaka';
+// console.log(capital.length);
+// console.log(capital[0]);
+//string is immutable means not change able
+capital[0]='L';
+console.log(capital);
