19bbc7ba129b378ac878f06bdfdea9a155b89309
sadhin28 <taosifsadhin113@gmail.com>
2025-03-29 01:54:41 +0600

    task4 done

---
+++ task/task4.js
@@ -0,0 +1,25 @@
+//Count the number of properties.
+
+
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+}
+//using object.key
+
+
+// let propertyCount = Object.keys(student).length;
+// console.log(propertyCount)
+
+//using for in
+
+let count = 0;
+for (let key in student) {
+    if (student.hasOwnProperty(key)) {
+        count++;
+    }
+}
+console.log(count);
