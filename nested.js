4cfea3c14365d2cb4b846ae079445c30870e3695
sadhin28 <taosifsadhin113@gmail.com>
2025-03-28 23:14:53 +0600

    nested objects

---
+++ nested.js
@@ -0,0 +1,24 @@
+const college ={
+    name:'Govt Gouranadi College',
+    class:['11','12'],
+    events:[
+        'Science Fair',
+        'bijoy dibos',
+        '21 feb'
+    ],
+    unique:{
+        color:'blue',
+        result:{
+            gpa:5,
+            merit:'top'
+        }
+
+    }
+}
+// console.log(college.unique.color)
+college.unique.result.gpa=6
+college.unique.result.merit='low'
+console.log(college.unique.result.gpa)
+console.log(college.unique.result.merit)
+college.events[1]='16 December'
+console.log(college.events[1]);
