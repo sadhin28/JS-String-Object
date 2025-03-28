f0b206ca3ca9e96d9e4d1ad6eaa68a6052bd0dbf
sadhin28 <taosifsadhin113@gmail.com>
2025-03-29 01:40:33 +0600

    task3 done

---
+++ task/task3.js
@@ -0,0 +1,17 @@
+//Display the physics marks as output.
+
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+//way 1
+// let phy=student.physics.marks;
+// console.log(phy);
+
+//way 2
+console.log(student.physics.marks)
