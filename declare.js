40b6b880dfe3196d31e14c36f9d40212b1f85213
sadhin28 <taosifsadhin113@gmail.com>
2025-03-28 23:40:10 +0600

    declaration objects

---
+++ declare.js
@@ -0,0 +1,13 @@
+const pen ={
+    brand:'Matador',
+    price:'5 taka',
+    color:'blue'
+}
+const pencil = new Object()
+
+console.log(pencil)
+
+const rubber = Object.create({});
+console.log(rubber)
+
+//using new
