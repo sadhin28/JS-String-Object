9b3b3805445beb359e92349afaf2a016898e36ce
sadhin28 <taosifsadhin113@gmail.com>
2025-03-29 01:37:36 +0600

    task2.js

---
+++ task/task2.js
@@ -0,0 +1,10 @@
+//For this object below add a property named passenger capacity with value 5
+
+let car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    passenger:5
+};
+
+console.log(car)
