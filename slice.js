d74ac99dadf59ef0417e9c02598689f3d93b390d
sadhin28 <taosifsadhin113@gmail.com>
2025-03-28 16:01:02 +0600

    join

--- slice.js
+++ slice.js
@@ -4,3 +4,10 @@ console.log(part);
 
 const sentence = 'I am a good and hrdworking person';
 console.log(sentence.split(' '))
+
+const friendStr ='fahin,rahim,karim,dahim,lahim'
+const friends =friendStr.split(',');
+console.log(friends);
+
+const realfriend= ['fahin', 'rahim', 'karim', 'dahim', 'lahim']
+console.log(realfriend.join('|'))
