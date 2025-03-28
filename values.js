4cff6d77491ec91e821644244b9dc8877406d4c3
sadhin28 <taosifsadhin113@gmail.com>
2025-03-28 23:02:51 +0600

    object er value berkora sikhlam

---
+++ values.js
@@ -0,0 +1,10 @@
+const computer = {
+    brand:'Lenovo',
+    price:113000,
+    processor:'Ryzen',
+    hdd: '512gb'
+}
+const values = Object.values(computer);
+const keye  =Object.keys(computer);
+console.log(keye)
+console.log(values)
