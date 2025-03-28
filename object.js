384879ff7964cb6300be7b9878e6d94f8b0709a7
sadhin28 <taosifsadhin113@gmail.com>
2025-03-28 22:54:11 +0600

    property niya sikhlalm

---
+++ object.js
@@ -0,0 +1,29 @@
+const age = 21;
+const school = 'Rifel Square';
+const isPassed = true;
+let isDeveloper;
+const subjects = ['bangla', 'English', 'Physics', 'Math']
+console.log(isDeveloper)
+//object declaration
+
+const bottle = {
+    brand:'Pran',
+    price:45,
+    color:'white',
+    isClean:false
+
+}
+
+const subject = {
+    name:'biology',
+    teacher:'Dorbesh ali ',
+    examDate:'30 Feb',
+    chapter:['first','second', 'thired'],
+    exams:{
+        name:'final Exam',
+        marks:100
+    }
+    
+}
+console.log(subject)
+console.log(subject.exams)
---
+++ property.js
@@ -0,0 +1,18 @@
+const person ={
+    name:'Sadhin',
+    age:23,
+    progession:'Developer',
+    salary:55000,
+    marrid:false,
+    'fav places':['bandorbo', 'saintmartin']
+}
+//dot notation
+// console.log(person.age)
+const incom=person.salary;
+console.log(incom)
+
+//bracket Notation
+// console.log(person['age'])
+const boyos =person['age']
+console.log(boyos)
+console.log(person["fav places"])
