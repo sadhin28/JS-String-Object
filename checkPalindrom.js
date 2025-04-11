2b4741d9647591fe0d78e3e46046c6866c4a6d17
sadhin28 <taosifsadhin113@gmail.com>
2025-04-12 00:30:28 +0600

    check palindrom problem

---
+++ checkPalindrom.js
@@ -0,0 +1,8 @@
+function isPalindrome(str) {
+    let reversed = str.split('').reverse().join('');
+    return str === reversed;
+  }
+  
+  console.log(isPalindrome("madam")); // true
+  console.log(isPalindrome("hello")); // false
+  
